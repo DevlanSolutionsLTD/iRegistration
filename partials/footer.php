e3fd2f44c042984274e59cae704f36df0dfff2f8
MartMbithi <martdevelopers254@gmail.com>
2021-01-16 15:05:43 +0300

    Partialized Partials - Set Initial Project Files Structure

---
+++ partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+        iRegistration
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2020-<?php echo date('Y'); ?>. A <a href="https://devlan.martdev.info">DevLan Inc</a>. Production</strong> All rights reserved.
+</footer>
---
+++ partials/scripts.php
@@ -0,0 +1,3 @@
+<script src="../public/plugins/jquery/jquery.min.js"></script>
+<script src="../public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../public/dist/js/adminlte.min.js"></script>
