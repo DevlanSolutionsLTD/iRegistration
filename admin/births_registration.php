f3aa570f2fd87e6a07e68e5c60eea5be641a3816
MartMbithi <martdevelopers254@gmail.com>
2021-01-19 13:57:51 +0300

    Get List Of All Registered Births

--- admin/births_registration.php
+++ admin/births_registration.php
@@ -5,6 +5,7 @@ require_once('../config/checklogin.php');
 require_once('../config/codeGen.php');
 
 /* Import Birth Registration Files From Excel Sheets */
+
 use DevLanDataAPI\DataSource;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 
@@ -82,12 +83,12 @@ if (isset($_POST["upload"])) {
             if (isset($spreadSheetAry[$i][8])) {
                 $year_reg = mysqli_real_escape_string($conn, $spreadSheetAry[$i][8]);
             }
-            
+
             $created_at = date('d M Y');
 
             //Get A System Generated Birth Registration Number
-            $reg_number = $alpha-$beta;
-            
+            $reg_number = $alpha - $beta;
+
             if (!empty($name) || !empty($dob) || !empty($place_of_birth) || !empty($dob) || !empty($sex)) {
                 $query = "INSERT INTO births_registration (id, reg_number, name, dob, sex, fathers_name, mothers_name, place_of_birth, month_reg, year_reg, created_at) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
                 $paramType = "sssssssssss";
@@ -140,7 +141,7 @@ if (isset($_POST['add_birth'])) {
         $error = 1;
         $err = "Gender Cannot Be Empty";
     }
-    
+
     /* System Generated Registration Number -> To Edit This File Check Codegen.php File Under Configs*/
     if (isset($_POST['reg_number']) && !empty($_POST['reg_number'])) {
         $reg_number = mysqli_real_escape_string($mysqli, trim($_POST['reg_number']));
@@ -148,7 +149,7 @@ if (isset($_POST['add_birth'])) {
         $error = 1;
         $err = "Registration Number Cannot Be Empty";
     }
-    
+
     if (!$error) {
         //prevent Double entries
         $sql = "SELECT * FROM  births_registration WHERE  reg_number='$reg_number'  ";
@@ -188,7 +189,7 @@ if (isset($_POST['add_birth'])) {
 
 /* Update Birth */
 if (isset($_POST['update_birth'])) {
-   /* Handle Birth Records Update Logic */
+    /* Handle Birth Records Update Logic */
 }
 
 if (isset($_GET['delete_birth'])) {
@@ -281,9 +282,25 @@ require_once('../partials/head.php');
                                     <form method="post" enctype="multipart/form-data" role="form">
                                         <div class="card-body">
                                             <div class="row">
-                                                <div class="form-group col-md-4">
+                                                <div class="form-group col-md-6">
                                                     <label for="">Registration Number</label>
-                                                    <input type="text" required name="reg_number" value="<?php echo $alpha;?>-<?php echo $beta;?>" class="form-control" id="exampleInputEmail1">
+                                                    <input type="text" required name="reg_number" value="<?php echo $alpha; ?>-<?php echo $beta; ?>" class="form-control" id="exampleInputEmail1">
+                                                </div>
+                                                <div class="form-group col-md-6">
+                                                    <label for="">Registrar Name</label>
+                                                    <select type="text" required name="registrar_name" class="form-control basic">
+                                                        <?php
+                                                        /* Pull A List Of All Registers */
+                                                        $ret = "SELECT * FROM `users` ";
+                                                        $stmt = $mysqli->prepare($ret);
+                                                        $stmt->execute(); //ok
+                                                        $res = $stmt->get_result();
+                                                        $cnt = 1;
+                                                        while ($users = $res->fetch_object()) {
+                                                        ?>
+                                                            <option><?php echo $users->name; ?></option>
+                                                        <?php } ?>
+                                                    </select>
                                                 </div>
                                                 <div class="form-group col-md-4">
                                                     <label for="">Child Full Name</label>
@@ -323,7 +340,7 @@ require_once('../partials/head.php');
                                             </div>
                                         </div>
                                         <div class="text-right">
-                                            <button type="submit" name="add_registrar" class="btn btn-primary">Submit</button>
+                                            <button type="submit" name="add_birth" class="btn btn-primary">Submit</button>
                                         </div>
                                     </form>
                                 </div>
@@ -341,91 +358,50 @@ require_once('../partials/head.php');
                             <table id="dt-1" class="table table-bordered table-striped">
                                 <thead>
                                     <tr>
-                                        <th>Full Name</th>
-                                        <th>ID No</th>
+                                        <th>Reg No</th>
+                                        <th>Name</th>
+                                        <th>DOB</th>
                                         <th>Gender</th>
-                                        <th>Email</th>
-                                        <th>Phone No</th>
-                                        <th>Address</th>
+                                        <th>Father</th>
+                                        <th>Mother</th>
+                                        <th>Place Of Birth</th>
                                         <th>Manage</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <?php
-                                    $ret = "SELECT * FROM `users` ";
+                                    $ret = "SELECT * FROM `births_registration` ";
                                     $stmt = $mysqli->prepare($ret);
                                     $stmt->execute(); //ok
                                     $res = $stmt->get_result();
                                     $cnt = 1;
-                                    while ($users = $res->fetch_object()) {
+                                    while ($births = $res->fetch_object()) {
                                     ?>
                                         <tr>
-                                            <td><?php echo $users->name; ?></td>
-                                            <td><?php echo $users->national_idno; ?></td>
-                                            <td><?php echo $users->sex; ?></td>
-                                            <td><?php echo $users->email; ?></td>
-                                            <td><?php echo $users->phone; ?></td>
-                                            <td><?php echo $users->addr; ?></td>
+                                            <td><?php echo $births->reg_number; ?></td>
+                                            <td><?php echo $births->name; ?></td>
+                                            <td><?php echo $births->dob; ?></td>
+                                            <td><?php echo $births->sex; ?></td>
+                                            <td><?php echo $births->fathers_name; ?></td>
+                                            <td><?php echo $births->mothers_name; ?></td>
+                                            <td><?php echo $births->place_of_birth; ?></td>
                                             <td>
-                                                <a class="badge badge-primary" data-toggle="modal" href="#update-<?php echo $users->id; ?>">
+                                                <a class="badge badge-primary" data-toggle="modal" href="#update-<?php echo $births->id; ?>">
                                                     <i class="fas fa-edit"></i>
                                                     Update
                                                 </a>
-                                                <!-- Update Registrar Modal -->
-                                                <div class="modal fade" id="update-<?php echo $users->id; ?>">
+                                                <!-- Update Births Modal -->
+                                                <div class="modal fade" id="update-<?php echo $births->id; ?>">
                                                     <div class="modal-dialog  modal-lg">
                                                         <div class="modal-content">
                                                             <div class="modal-header">
-                                                                <h4 class="modal-title">Update <?php echo $users->name; ?> Profile</h4>
+                                                                <h4 class="modal-title">Update <?php echo $births->reg_number; ?> Record</h4>
                                                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                                     <span aria-hidden="true">&times;</span>
                                                                 </button>
                                                             </div>
                                                             <div class="modal-body">
-                                                                <form method="post" enctype="multipart/form-data" role="form">
-                                                                    <div class="card-body">
-                                                                        <div class="row">
-                                                                            <div class="form-group col-md-6">
-                                                                                <label for="">Name</label>
-                                                                                <input type="text" required name="name" value="<?php echo $users->name; ?>" class="form-control" id="exampleInputEmail1">
-                                                                                <input type="hidden" required name="id" value="<?php echo $users->id; ?>" class="form-control">
-                                                                            </div>
-                                                                            <div class="form-group col-md-6">
-                                                                                <label for="">ID / Passport Number</label>
-                                                                                <input type="text" required name="national_idno" value="<?php echo $users->national_idno; ?>" class="form-control">
-                                                                            </div>
-                                                                        </div>
 
-                                                                        <div class="row">
-                                                                            <div class="form-group col-md-4">
-                                                                                <label for="">Email</label>
-                                                                                <input type="email" required name="email" value="<?php echo $users->email; ?>" class="form-control">
-                                                                            </div>
-                                                                            <div class="form-group col-md-4">
-                                                                                <label for="">Phone Number</label>
-                                                                                <input type="text" required name="phone" value="<?php echo $users->phone; ?>" class="form-control">
-                                                                            </div>
-                                                                            <div class="form-group col-md-4">
-                                                                                <label for="">Gender</label>
-                                                                                <select type="text" required name="sex" class="form-control basic">
-                                                                                    <option><?php echo $users->sex; ?></option>
-                                                                                    <option>Male</option>
-                                                                                    <option>Female</option>
-                                                                                </select>
-                                                                            </div>
-                                                                        </div>
-
-                                                                        <div class="row">
-                                                                            <div class="form-group col-md-12">
-                                                                                <label for="exampleInputPassword1">Address</label>
-                                                                                <textarea required name="addr" rows="5" class="form-control"><?php echo $users->addr; ?></textarea>
-                                                                            </div>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="text-right">
-                                                                        <button type="submit" name="update_registrar" class="btn btn-primary">Submit</button>
-                                                                    </div>
-                                                                </form>
                                                             </div>
                                                             <div class="modal-footer justify-content-between">
                                                                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -435,12 +411,12 @@ require_once('../partials/head.php');
                                                 </div>
                                                 <!-- End Modal -->
 
-                                                <a class="badge badge-danger" data-toggle="modal" href="#delete-<?php echo $users->id; ?>">
+                                                <a class="badge badge-danger" data-toggle="modal" href="#delete-<?php echo $births->id; ?>">
                                                     <i class="fas fa-trash"></i>
                                                     Delete
                                                 </a>
                                                 <!-- Delete Confirmation Modal -->
-                                                <div class="modal fade" id="delete-<?php echo $users->id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal fade" id="delete-<?php echo $births->id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                                     <div class="modal-dialog modal-dialog-centered" role="document">
                                                         <div class="modal-content">
                                                             <div class="modal-header">
@@ -450,10 +426,10 @@ require_once('../partials/head.php');
                                                                 </button>
                                                             </div>
                                                             <div class="modal-body text-center text-danger">
-                                                                <h4>Delete <?php echo $users->name; ?> ?</h4>
+                                                                <h4>Delete <?php echo $births->name; ?> - <?php echo $births->reg_number; ?> ?</h4>
                                                                 <br>
                                                                 <button type="button" class="text-center btn btn-success" data-dismiss="modal">No</button>
-                                                                <a href="registras.php?delete=<?php echo $users->id; ?>" class="text-center btn btn-danger"> Delete </a>
+                                                                <a href="births_registration.php?delete=<?php echo $births->id; ?>" class="text-center btn btn-danger"> Delete </a>
                                                             </div>
                                                         </div>
                                                     </div>
