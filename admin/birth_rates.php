e743f3c3819e21e2c11658b03de8b7cd7604cc27
MartMbithi <martdevelopers254@gmail.com>
2021-01-22 11:33:48 +0300

    Birth Reports - With Custom Header Tag

--- admin/birth_rates.php
+++ admin/birth_rates.php
@@ -3,10 +3,32 @@ session_start();
 require_once('../config/config.php');
 require_once('../config/checklogin.php');
 require_once('../config/codeGen.php');
-
-require_once('../partials/head.php');
-
 ?>
+<!DOCTYPE html>
+
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>iRegistration - Births Reports</title>
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="../public/plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../public/dist/css/adminlte.min.css">
+    <!-- Bootstrap Select CSS -->
+    <link rel="stylesheet" href="../public/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="../public/plugins/datatables-bs4/css/dataTables.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../public/plugins/datatable/custom_dt_html5.css">
+
+
+</head>
 
 <body class="hold-transition layout-top-nav">
     <div class="wrapper">
